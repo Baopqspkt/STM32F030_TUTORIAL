8b1cab3e3ea1affc066a019fd165ea976ce9444e
Baopqspkt <baopq.spkt@gmail.com>
2021-04-24 01:16:14 +0700

    Add I2c DMA

---
+++ CUBEMX/STM32F030_I2C_DMA/Core/Inc/i2c.h
@@ -0,0 +1,16 @@
+/*
+ * i2c.h
+ *
+ *  Created on: Apr 23, 2021
+ *      Author: PC
+ */
+
+#include "main.h"
+
+//void i2c_write(I2C_HandleTypeDef *hi2c,uint8_t slave_addr, uint8_t* send_data);
+void i2c_write(I2C_HandleTypeDef *hi2c,uint8_t slave_addr, uint8_t addr_register, uint8_t send_data);
+uint8_t i2c_read(I2C_HandleTypeDef *hi2c,uint8_t slave_addr, uint8_t register_addr);
+
+//----------------------------- These function below use by DMA -------------------------//
+void i2c_write_dma(I2C_HandleTypeDef *hi2c,uint8_t slave_addr, uint8_t addr_register, uint8_t send_data);
+
